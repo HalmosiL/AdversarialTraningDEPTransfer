0e9658a5e6c0709fd866334d7e8777d9b57e362b
HalmosiL <110020368+HalmosiL@users.noreply.github.com>
2022-09-17 20:06:25 +0200

    Update Resnet.py

--- models/Resnet.py
+++ models/Resnet.py
@@ -202,8 +202,8 @@ def resnet50(pretrained=False, **kwargs):
     model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
     if pretrained:
         #model.load_state_dict(model_zoo.load_url(model_urls['resnet50']))
-        model.load_state_dict(torch.load(model_paths["resnet50"]), strict=False)
-    return model.to("cpu")
+        model.load_state_dict(torch.load(model_paths["resnet50"], map_location=torch.device('cpu')), strict=False)
+    return model
 
 
 def resnet101(pretrained=False, **kwargs):
